86641eba10c2d54d9d8b6f956559811942cdc54f
Taco van Dijk <taco@waag.org>
2021-07-29 14:43:04 +0200

    auto line break labels at 10 char limit

--- graphviz/dsl.rb
+++ graphviz/dsl.rb
@@ -1,16 +1,17 @@
 #dsl for generating value flow diagrams with graphviz
+LABEL_WIDTH = 10 # max characters
 
 # print an agent
 def agent(id, label)
-  puts "#{id} [label=\"#{label.gsub("\n","\\n")}\" fixedsize=true, width=1.25, shape=circle, style=\"filled\", fillcolor=\"#43e272\", color=\"#4cb46c\" fontname=\"Helvetica\"];"
+  puts "#{id} [label=\"#{insert_breaks(label.gsub("\n","\\n"))}\" fixedsize=true, width=1.25, shape=circle, style=\"filled\", fillcolor=\"#43e272\", color=\"#4cb46c\" fontname=\"Helvetica\"];"
 end
 
 def resource(id, label)
-  puts "#{id} [label=\"#{label.gsub("\n","\\n")}\", fixedsize=true, width=1.1 shape=square, style=\"filled\", fillcolor=\"#004576\", fontcolor=\"white\", color=\"#697d8a\" fontname=\"Helvetica\"];"
+  puts "#{id} [label=\"#{insert_breaks(label.gsub("\n","\\n"))}\", fixedsize=true, width=1.1 shape=square, style=\"filled\", fillcolor=\"#004576\", fontcolor=\"white\", color=\"#697d8a\" fontname=\"Helvetica\"];"
 end
 
 def event(id, label)
-  puts "#{id} [label=\"#{label.gsub("\n","\\n")}\", fixedsize=true, width = 1.1, shape=square, style=\"rounded,filled\", fillcolor=\"#e2e2e2\", color=\"#b5b5b5\" fontname=\"Helvetica\"];"
+  puts "#{id} [label=\"#{insert_breaks(label.gsub("\n","\\n"))}\", fixedsize=true, width = 1.1, shape=square, style=\"rounded,filled\", fillcolor=\"#e2e2e2\", color=\"#b5b5b5\" fontname=\"Helvetica\"];"
 end
 
 def role(event, agent, role)
@@ -19,6 +20,23 @@ end
 
 def flow(ids)
   puts ids.join(" -> ") 
+end
+
+def insert_breaks(labeltext)
+	words = labeltext.split(' ')
+	newtext = []
+	newline = ""
+	words.each do | word |
+		if (newline.length> 0 and
+		  newline.length + word.length > LABEL_WIDTH)
+		  newtext << newline		 	
+		  newline = ""
+		end
+		newline += " " if newline.length> 0
+		newline += word 
+	end
+	newtext << newline if newline.length> 0
+	newtext.join("\n")
 end
 
 def graph(label)
