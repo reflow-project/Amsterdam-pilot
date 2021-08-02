39383200bedd66ea807ed43167f7c3cd8d68f3cf
Taco van Dijk <taco@waag.org>
2021-08-02 13:36:19 +0200

    updated zorgshchorten second part

--- graphviz/zorgschorten.rb
+++ graphviz/zorgschorten.rb
@@ -2,6 +2,7 @@ require_relative 'dsl.rb'
 
 graph("Zorgschorten") {
 
+  #use cycle
   agent :a_hospital, "Hospital"
   agent :a_laundry, "Laundry Service"
   agent :a_tsc, "Textile Service Company"
@@ -30,4 +31,42 @@ graph("Zorgschorten") {
   role :e_use, :a_hospital, "user"
 
   flow [:r_gown_dirty,:e_pickup,:r_gown_dirty2,:e_work,:r_gown_clean,:e_pickup_tcs,:r_gown_clean2,:e_qi,:r_gown_inv_up,:e_pickup_hospital,:r_gown_inv_down,:e_use,:r_gown_dirty]
+
+  # 1. sorting and incineration flow
+  agent :a_sorter, "Sorting Center"
+  agent :a_incinerator, "Incinerator"
+  
+  resource :r_gown_dep, "Gown Lot (depreciated)"
+  resource :r_unsorted, "Unsorted Material Vol (kg)"
+  resource :r_waste, "Waste (Volume, kg)"
+
+  # Tcs transfer depreciated gowns to sorting center
+  event :e_transfer_sort, "Transfer (Delivery)"
+  role :e_transfer_sort, :a_tsc, "Provider"
+  role :e_transfer_sort, :a_sorter, "Receiver"
+
+  # sorting senter sorts gowns to recylable material and waste
+  event :e_sort, "Consume / Produce (Sort)"
+  role :e_sort, :a_sorter, "Operator"
+
+  # waste transfered to incinerator
+  event :e_transfer_waste, "Transfer"
+  role :e_transfer_waste, :a_sorter, "Provider"
+  role :e_transfer_waste, :a_incinerator, "Receiver"
+ 
+  # incinerator consumes waste
+  event :e_incinerate, "Consume (Incinerate)"
+  role :e_incinerate, :a_incinerator, "Operator"
+  
+  flow [:e_qi, :r_gown_dep, :e_transfer_sort, :r_unsorted, :e_sort, :r_waste, :e_transfer_waste, :r_waste, :e_incinerate]
+
+  # 3. production flow
+  
+  # recyleable material is transferred from sorting center to unraveling agent
+  # unraveling agent recycles to unraveled material
+  # unraveled material is transfered to textile company
+  # textile company produces textile from unraveled material
+  # textile is transferred from textile company to confectie atelier
+  # confectie atelier produces new gown lot
+  # gown lot is sold to hospital
 }
