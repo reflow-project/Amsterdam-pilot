636a970e817ce4c186fbb3b90bd6ee6e179a8f65
Taco van Dijk <taco@waag.org>
2021-08-10 17:04:19 +0200

    sub graphs swap shop

--- graphviz/swapshop.rb
+++ graphviz/swapshop.rb
@@ -19,73 +19,81 @@ graph("Swapshop"){
   resource :r_unraveled, "Unraveled material"
   resource :r_wearable2, "Wearable clothing (Vol)"
 
-  # consumer brings in discarded items to swapshop
-  event :e_swap_in, "Transfer (swap in)"
-  event :e_sort_swapshop, "Consume / Produce (sorting)"
-  role :e_swap_in, :a_swapshop, "Receiver"
-  role :e_swap_in, :a_consumer, "Provider"
-  flow [:r_wearable2, :e_swap_in, :r_unsorted_swapshop, :e_sort_swapshop] 
- 
-  # swapshop sorts incoming stuff
-  role :e_sort_swapshop, :a_swapshop, "Operator"
- 
-  # swapshop puts in rack, most items are swapped out
-  event :e_swap_out, "Transfer (swap out)"
-  role :e_swap_out, :a_swapshop, "Provider"
-  role :e_swap_out, :a_consumer, "Receiver"
-  flow [:e_sort_swapshop, :r_wearable1, :e_swap_out, :r_wearable2] 
+  sub_graph("Use cycle") do
+    # consumer brings in discarded items to swapshop
+    event :e_swap_in, "Transfer (swap in)"
+    event :e_sort_swapshop, "Consume / Produce (sorting)"
+    role :e_swap_in, :a_swapshop, "Receiver"
+    role :e_swap_in, :a_consumer, "Provider"
+    flow [:r_wearable2, :e_swap_in, :r_unsorted_swapshop, :e_sort_swapshop] 
 
-  # the citizen uses the wearable clothing
-  role :e_wear, :a_consumer, "User"
-  event :e_wear, "Use (wear)"
-  flow [:r_wearable2, :e_wear, :r_wearable2]
- 
-  # the citizen maintains the wearable clothing
-  role :e_care, :a_consumer, "Maintainer"
-  event :e_care, "Modify (care/maintain)"
-  flow [:r_wearable2, :e_care, :r_wearable2]
- 
-  # the citizen has a repairs shop fix some wearable clothing
-  role :e_repair, :a_repairshop, "Operator"
-  event :e_repair, "Modify (repair/refurb)"
-  flow [:r_wearable2, :e_repair, :r_wearable2]
+    # swapshop sorts incoming stuff
+    role :e_sort_swapshop, :a_swapshop, "Operator"
 
-  # the citizen discards to trash
-  event :e_cons_trash, "Transfer (trash)"
-  role :e_cons_trash, :a_consumer, "Provider"
-  role :e_cons_trash, :a_incinerator, "Receiver"
-  flow [:r_wearable2, :e_cons_trash, :r_waste2]
+    # swapshop puts in rack, most items are swapped out
+    event :e_swap_out, "Transfer (swap out)"
+    role :e_swap_out, :a_swapshop, "Provider"
+    role :e_swap_out, :a_consumer, "Receiver"
+    flow [:e_sort_swapshop, :r_wearable1, :e_swap_out, :r_wearable2] 
 
-  # the citizen discards to recycle bin
-  event :e_cons_recycle, "Transfer (recycle)"
-  role :e_cons_recycle, :a_consumer, "Provider"
-  role :e_cons_recycle, :a_sorter, "Receiver"
-  flow [:r_wearable2, :e_cons_recycle, :r_unsorted_sorter] 
+    # the citizen uses the wearable clothing
+    role :e_wear, :a_consumer, "User"
+    event :e_wear, "Use (wear)"
+    flow [:r_wearable2, :e_wear, :r_wearable2]
 
-  # swapshop sends unwearable stuff to sorting company
-  event :e_depreciate, "Transfer (depreciate)"
-  role :e_depreciate, :a_swapshop, "Provider"
-  role :e_depreciate, :a_sorter, "Receiver"
-  flow [:e_sort_swapshop, :r_unwearable, :e_depreciate, :r_unsorted_sorter, :e_sort_sc] 
-  
-  #sorting company deems some stuff is waste, which goes on to the incinerator
-  event :e_incinerate, "Consume (incinerate)"
-  event :e_sort_sc, "Consume / Produce (sorting)"
-  event :e_discard_sc, "Transfer (discard)"
-  role :e_sort_sc, :a_sorter, "Operator"
-  role :e_discard_sc, :a_sorter, "Provider"
-  role :e_discard_sc, :a_incinerator, "Receiver"
-  role :e_incinerate, :a_incinerator, "Operator"
-  flow [:e_sort_sc, :r_waste1, :e_discard_sc, :r_waste2, :e_incinerate]  
+    # the citizen maintains the wearable clothing
+    role :e_care, :a_consumer, "Maintainer"
+    event :e_care, "Modify (care/maintain)"
+    flow [:r_wearable2, :e_care, :r_wearable2]
 
-  #sorting company deems some stuff recycleable, which goes on to the unraveler, which unravels (recyclces), and sells to tghe textile industry
-  event :e_recycle, "Consume / Produce (recycle)"
-  event :e_unraveler_sell, "Transfer (sell)"
-  event :e_sorter_sell, "Transfer (sell)"
-  role :e_sorter_sell, :a_sorter, "Provider"
-  role :e_sorter_sell, :a_unraveler, "Receiver"
-  role :e_recycle, :a_unraveler, "Operator"
-  role :e_unraveler_sell, :a_unraveler, "Provider"
-  role :e_unraveler_sell, :a_ti, "Receiver"
-  flow [:e_sort_sc, :r_recyclable, :e_sorter_sell, :r_recyclable2, :e_recycle, :r_unraveled, :e_unraveler_sell ] 
+    # the citizen has a repairs shop fix some wearable clothing
+    role :e_repair, :a_repairshop, "Operator"
+    event :e_repair, "Modify (repair/refurb)"
+    flow [:r_wearable2, :e_repair, :r_wearable2]
+  end
+
+  sub_graph("waste") do
+    # the citizen discards to trash
+    event :e_cons_trash, "Transfer (trash)"
+    role :e_cons_trash, :a_consumer, "Provider"
+    role :e_cons_trash, :a_incinerator, "Receiver"
+    flow [:r_wearable2, :e_cons_trash, :r_waste2]
+
+    #sorting company deems some stuff is waste, which goes on to the incinerator
+    event :e_incinerate, "Consume (incinerate)"
+    event :e_sort_sc, "Consume / Produce (sorting)"
+    event :e_discard_sc, "Transfer (discard)"
+    role :e_sort_sc, :a_sorter, "Operator"
+    role :e_discard_sc, :a_sorter, "Provider"
+    role :e_discard_sc, :a_incinerator, "Receiver"
+    role :e_incinerate, :a_incinerator, "Operator"
+    flow [:e_sort_sc, :r_waste1, :e_discard_sc, :r_waste2, :e_incinerate]  
+  end
+
+  sub_graph("recycle") do
+
+    # the citizen discards to recycle bin
+    event :e_cons_recycle, "Transfer (recycle)"
+    role :e_cons_recycle, :a_consumer, "Provider"
+    role :e_cons_recycle, :a_sorter, "Receiver"
+    flow [:r_wearable2, :e_cons_recycle, :r_unsorted_sorter] 
+
+    # swapshop sends unwearable stuff to sorting company
+    event :e_depreciate, "Transfer (depreciate)"
+    role :e_depreciate, :a_swapshop, "Provider"
+    role :e_depreciate, :a_sorter, "Receiver"
+    flow [:e_sort_swapshop, :r_unwearable, :e_depreciate, :r_unsorted_sorter, :e_sort_sc] 
+
+    #sorting company deems some stuff recycleable, which goes on to the unraveler, which unravels (recyclces), and sells to tghe textile industry
+    event :e_recycle, "Consume / Produce (recycle)"
+    event :e_unraveler_sell, "Transfer (sell)"
+    event :e_sorter_sell, "Transfer (sell)"
+    role :e_sorter_sell, :a_sorter, "Provider"
+    role :e_sorter_sell, :a_unraveler, "Receiver"
+    role :e_recycle, :a_unraveler, "Operator"
+    role :e_unraveler_sell, :a_unraveler, "Provider"
+    role :e_unraveler_sell, :a_ti, "Receiver"
+    flow [:e_sort_sc, :r_recyclable, :e_sorter_sell, :r_recyclable2, :e_recycle, :r_unraveled, :e_unraveler_sell ] 
+
+  end
 }
