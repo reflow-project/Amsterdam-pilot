b2536381a71a94153e830e999c5619d268fd0193
Taco van Dijk <taco@waag.org>
2021-08-17 16:41:48 +0200

    skeleton

--- sim/dsl.rb
+++ sim/dsl.rb
@@ -1,12 +1,26 @@
 #dsl for generating value flow simulation 
+module Resource
+  Pool = "RES_POOL" 
+  Inventory = "RES_INVENTORY" 
+  Lot = "RES_LOT" 
+end
+
+module Role
+  Provider = "ROLE_PROVIDER"
+  Receiver = "ROLE_RECEIVER"
+  Performer = "ROLE_PERFORMER"
+end
 
 # print an agent
 def agent(id, label)
   puts "agent: #{id} #{label}"
+  yield if block_given?
 end
 
-def resource(id, label)
+def resource(id, label, type = nil, amount = nil)
   puts "resource: #{id} #{label}"
+  puts "type: #{type}" if type != nil
+  puts "amount: #{amount}" if amount != nil
 end
 
 def event(id, label)
@@ -28,3 +42,28 @@ def graph(label)
   yield
 end
 
+def init 
+  puts "init"
+  yield
+end
+
+# cron specifies frequency in days, amount is an amount to generate, 
+# on specifes the name of an event that triggers the event, with_delay delays by a number of days
+def schedule(cron = nil, amount = nil, on = nil, with_delay = nil) 
+  puts "schedule"
+  yield
+end
+
+# perform a consume verb, if a fraction is specified operate on part of the batch, 
+# default operate on everything
+def consume(resource_key, fraction = 1.0)
+  puts "perform consume #{resource_key}"
+end
+
+def produce(resource_key, fraction = 1.0)
+  puts "perform produce #{resource_key}"
+end
+
+def transfer(resource_key, fraction = 1.0)
+  puts "perform transfer #{resource_key}"
+end
--- sim/zorgschorten.rb
+++ sim/zorgschorten.rb
@@ -9,18 +9,25 @@ graph("Zorgschorten") do
   # (maybe in reflow_os it's just individual gowns)
   # Resource::Inventory should exist as an inventory in reflow_os
   # Resource::Lot is group of items that can be transferred
-  #
-  agent :a_hospital, "Hospital" do
-    resource :gown_in_use, "Gown Lot (in use)", Resource::Pool, "400-500" # gowns in use in the hospital 
-    resource :gown_dirty_pool, "Gown (dirty)", Resource::Pool, "0" # gowns in the hamper in the hospital 
-  end
   
-  agent :a_tsc, "Textile Service Company" do 
-    resource :gown_stock, "Gown (in stock)", Resource::Inventory, "1000" # in stock in the tsc
+  # OLVG
+  agent :a_hospital, "OLVG" do
+    init do
+      resource :gown_in_use, "Gown Lot (in use)", Resource::Pool, "400-500" # gowns in use in the hospital 
+      resource :gown_dirty_pool, "Gown (dirty)", Resource::Pool, "0" # gowns in the hamper in the hospital 
+    end
+  end
+ 
+  # Clean Lease
+  agent :a_tsc, "Clean Lease Service" do 
+    init do
+      resource :gown_stock, "Gown (in stock)", Resource::Inventory, "1000" # in stock in the tsc
+    end
   end
 
   #this agent is considered 'jit', and only deals with lots
-  agent :a_launderer, "Laundry Service"
+  # Clean Lease
+  agent :a_launderer, "Clean Lease Laundry Service"
 
   #resources that have now owner, used for transfer
   resource :gown_dirty_lot, "Gown Lot (dirty)", Resource::Lot  
@@ -30,7 +37,7 @@ graph("Zorgschorten") do
   # on delivery add the lot to the in use pool 
   # every day between 10 and 40 in use gowns are put in the hamper
   event :e_out_use, "Use (Discard)" do 
-    simulation(:cron => "*/1" :amount => "10-40") do
+    schedule :cron => "*/1", :amount => "10-40" do
       role :a_hospital, Role::Performer
       consume :gown_in_use # lower / remove resources from the in use pool
       produce :gown_dirty_pool # raise / add resources to  the dirty pool
@@ -39,7 +46,7 @@ graph("Zorgschorten") do
 
   # every seven days the dirty pool is picked up
   event :e_transfer_pickup, "pickup" do 
-    simulate :cron => "*/7" do
+    schedule :cron => "*/7" do
       role :a_hospital, Role::Provider 
       role :a_launderer, Role::Receiver 
       consume :gown_dirty_pool # lowers the pool to 0
@@ -49,17 +56,19 @@ graph("Zorgschorten") do
   end 
 
   # performs laundry for entire lot 1 day abter receiving :gown_dirty_lot
-  event :e_laundry, "Work (laundry)" do
-    simulate :on => :e_transfer_pickup, :with_delay => "1d" do
+  event :e_laundry, "Work (do laundry)" do
+    schedule :on => :e_transfer_pickup, :with_delay => "1d" do
       role :a_launderer, Role::Performer 
       consume :gown_dirty_lot
       produce :gown_clean
+
+      # the nr of time that the gown is washed should be updated
     end
   end 
 
   # transfers entire lot between 0-2 days after cleaning to tsc
   event :e_transfer_clean, "Transfer" do 
-    simulate (:on => :e_laundry, :with_delay =>"0-2d") do
+    schedule :on => :e_laundry, :with_delay => "0-2d"  do
       role :a_launderer, Role::Provider 
       role :a_tsc, Role::Receiver 
       transfer :gown_clean
@@ -68,17 +77,17 @@ graph("Zorgschorten") do
 
   # tcs inspect lot between 0-3 days after receiving
   event :e_inspect, "Modify (QI)" do 
-    simulate(:on => :e_transfer_clean, :with_delay => "0-3d") do
+    schedule :on => :e_transfer_clean, :with_delay => "0-3d" do
       role :a_tsc, Role::Performer 
-      consume :gown_clean, "1.0f" 
-      produce :gown_stock, "0.8-0.95f" # between 0.05 and 0.2 of the lot doesn't pass inspection (should raise inventory)
+      consume :gown_clean, "1.0" 
+      produce :gown_stock, "0.8-0.95" # between 0.05 and 0.2 of the lot doesn't pass inspection (should raise inventory)
     end
   end  
 
   # every day, take between 1 and 10 gowns from 
   # the TSC stock and transfer them to the hospital
   event :e_transfer_delivery, "Transfer (Delivery)" do 
-    simulation(:cron => "*/1", :amount => "1-10") do
+    schedule :cron => "*/1", :amount => "1-10" do
       role :a_tsc, Role::Provider 
       role :a_hospital, Role::Receiver 
       consume :gown_stock
@@ -89,7 +98,7 @@ graph("Zorgschorten") do
 
   # on delivery add the lot to the in use pool 
   event :e_in_use, "Use (Wear)" do 
-    simulation(:on => :e_transfer_delivery) do
+    schedule :on => :e_transfer_delivery do
       role :a_hospital, Role::Performer
       consume :gown_ready_for_use
       produce :gown_in_use #raise the in use pool
