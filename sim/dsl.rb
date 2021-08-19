d8be4e3a4773523154a4f8c4daf0222927082b0b
Taco van Dijk <taco@waag.org>
2021-08-19 15:48:08 +0200

    execute first event

--- sim/dsl.rb
+++ sim/dsl.rb
@@ -1,32 +1,141 @@
-#dsl for generating value flow simulation 
-module Resource
-  Pool = "RES_POOL" 
-  Inventory = "RES_INVENTORY" 
-  Lot = "RES_LOT" 
-end
+require 'date'
 
+#dsl for generating value flow simulation 
 module Role
   Provider = "ROLE_PROVIDER"
   Receiver = "ROLE_RECEIVER"
   Performer = "ROLE_PERFORMER"
 end
 
-# print an agent
-def agent(id, label)
-  puts "agent: #{id} #{label}"
+# this simulation first generate all the necessary operations in a setup phase and a timetable
+# then executes the setup phase and when it's done the timetable on entry at a time using graphql requests with delays in between
+def simulation(label, date_start = Date.today, date_end = Date.today + 365)
+  puts "#{date_start} -> #{date_end}"
+  $planned_events = [] #array of days, each do contains array of event keys to process on that day
+  $nr_of_days = (date_end - date_start).to_i
+  $nr_of_days.times do  |index|
+    $planned_events[index] = [] #no events planned
+  end
+  yield #setup / generate agents, resources and schedule events
+
+  # show internal state after setup
+  puts "--- SETUP ---"
+  puts "RESOURCES"
+  puts $resources
+  puts "LOTS"
+  puts $lots
+  puts "POOLS"
+  $pools.keys.each do |key|
+    puts "#{key}: #{$pools[key][:items].count} (#{$pools[key][:agent_key]})"
+  end
+  puts "INVENTORIES"
+  $inventories.keys.each do |key|
+    puts "#{key}: #{$inventories[key][:items].count} (#{$inventories[key][:agent_key]})"
+  end
+  puts "AGENTS"
+  puts $agents
+  puts "EVENTS"
+  #pp $planned_events
+  #puts $processes
+
+  puts "--- RUN SIMULATION ---"
+  $nr_of_days.times do  |index|
+    day = date_start + index #no events planned
+    $planned_events[index].each do |event_key|
+      puts "#{day}: processing event: #{event_key}"
+      #execute the process
+      $processes[event_key].call
+      sleep 0.1 #delay not to go to fast
+    end
+  end
+
+end
+
+# setup an agent
+$context = {}
+$agents = Hash.new
+def agent(key, label)
+  $agents[key] = {:label => label}
+  $context[:agent_key] = key #used as context for block 
   yield if block_given?
 end
 
-def resource(id, label, type = nil, amount = nil)
-  puts "resource: #{id} #{label}"
-  puts "type: #{type}" if type != nil
-  puts "amount: #{amount}" if amount != nil
+# setup a resource type e.g. gown, 
+# expects a block that can be used to generate a resource instance, a hash that contains two keys
+# :rid and :description
+$resources = Hash.new 
+def resource(key, label, &proc)
+  $resources[key] = {:label => label, :generator => proc}
 end
 
-def event(id, label)
-  puts "event: #{id} #{label}"
-  $cur_id = id
+# a collection of resources that can be used for transfers
+# type refers to an id of a resource that has been defined as a resource 
+# amount for lot is always specified by the event
+# items will be added / removed inside events, not at definition time
+$lots = Hash.new
+def lot(key, label, resource_key)
+  raise "resource not found: #{resource_key}" if not $resources.key? resource_key 
+  $lots[key] = {:label => label, :resource_key => resource_key, :items => []}
+end
+
+# a collection of resources that we want to keep track of in the simulation 
+# (belongs to an agent)
+$pools = Hash.new
+def pool(key, label, resource_key, amount = 0)
+  raise "resource not found: #{type}" if not $resources.key? resource_key 
+  items = []
+  # generate the initial items 
+  if(amount > 0)
+      amount.times do 
+        item = $resources[resource_key][:generator].call
+        items << item
+      end
+  end
+  $pools[key] = {:label => label, :resource_key => resource_key, :items => items, :agent_key => $context[:agent_key] }
+end
+
+# a collection of resources that we want to keep track of in the simulation AND in reflow os 
+$inventories = Hash.new
+def inventory(key, label, resource_key, amount = 0)
+  raise "resource not found: #{type}" if not $resources.key? resource_key 
+  items = []
+  # generate the initial items 
+  if(amount > 0)
+      amount.times do 
+        item = $resources[resource_key][:generator].call
+        items << item
+      end
+  end
+  $inventories[key] = {:label => label, :resource_key => resource_key, :items => items, :agent_key => $context[:agent_key] }
+end
+
+$events = Hash.new
+def event(key, label)
+  $context[:event_key] = key 
+  $context[:event_label] = label 
   yield
+end
+
+# every specifies frequency in days, amount is an amount to generate, 
+# on specifes the name of an event that triggers the event, 
+# with_delay delays the start by a number of days
+def schedule(cron=nil, on=nil, with_delay=nil) 
+  event_key = $context[:event_key]
+  if(cron != nil)
+    $nr_of_days.times do |day_nr|
+      if (day_nr % cron == 0)
+        $planned_events[day_nr] << event_key 
+      end
+    end
+  end
+  #if cron is not nil, put the event key as value in the array of every day that matches 
+  # for each day number between start and end date
+end
+
+$processes = Hash.new 
+def process(&proc)
+  event_key = $context[:event_key]
+  $processes[event_key] = proc #save the process to execute when the event is scheduled 
 end
 
 def role(agent, role)
@@ -37,33 +146,31 @@ def flow(ids)
   puts ids.join(" -> ") 
 end
 
-def graph(label)
-  puts label
-  yield
+#set the amount for the current context for a verb
+def with_amount(amount)
+  $context[:process_amount] = amount
 end
 
-def init 
-  puts "init"
-  yield
-end
-
-# cron specifies frequency in days, amount is an amount to generate, 
-# on specifes the name of an event that triggers the event, with_delay delays by a number of days
-def schedule(cron = nil, amount = nil, on = nil, with_delay = nil) 
-  puts "schedule"
-  yield
+#set the amount for the current context for a verb
+def with_performer(agent)
+  $context[:process_performer] = agent 
 end
 
 # perform a consume verb, if a fraction is specified operate on part of the batch, 
 # default operate on everything
 def consume(resource_key, fraction = 1.0)
-  puts "perform consume #{resource_key}"
+  amount = fraction * $context[:process_amount]
+  performer = $context[:process_performer]
+  puts "perform consume #{amount} #{resource_key} by #{performer}"
 end
 
 def produce(resource_key, fraction = 1.0)
-  puts "perform produce #{resource_key}"
+  amount = fraction * $context[:process_amount]
+  performer = $context[:process_performer]
+  puts "perform produce #{amount} #{resource_key} by #{performer}"
 end
 
-def transfer(resource_key, fraction = 1.0)
-  puts "perform transfer #{resource_key}"
+def transfer(resource_key, fraction = 1.0, provider, receiver)
+  amount = fraction * $context[:process_amount]
+  puts "perform transfer #{resource_key} from #{provider} to #{receiver}"
 end
--- sim/zorgschorten.rb
+++ sim/zorgschorten.rb
@@ -1,108 +1,118 @@
 require_relative 'dsl.rb'
+require 'securerandom'
 
-graph("Zorgschorten") do 
+simulation("Zorgschorten") do 
 
-  #define agent and initial resources
-  #baseline state
-  # Resource::Pool is a collection of resources, tied to an agent that we need to keep track of in the simulation 
-  # but the collection does not necessarily exist as an explicit inventory in reflow_os
-  # (maybe in reflow_os it's just individual gowns)
-  # Resource::Inventory should exist as an inventory in reflow_os
-  # Resource::Lot is group of items that can be transferred
-  
+# set an optional start date and end date for the simulation
+# default is today as start date and 1 year of simulated events
+  # first we schedule 'root event' that have :cron set
+  # then we schedule 'dependent events' that have :on set
+  # the schedule is a table with dates as keys, and as value a generated struct that contains everything to perform an action on a resource by and agent
+  # the simulation should internally keep track of what individual resources are transferred from one pool to another
+  # use / clean cycle only for now
+ 
+  # gowns in use in the hospital 
+  resource :gown, "Gown" do
+    rid = SecureRandom.uuid
+    # this id is not the reflow os id, but the id used by cleanlease to track the invidual gown
+    {:rid => "http://cleanlease.nl/zs/#{rid}", :description => "Clean Lease Schort: #{rid}"}
+  end
+
+  # a lot might acutally have it's own id (and a list of included items as description) but for now we assumen they are anonymous batches (they do get their own id in reflow os though)
+  # we only have one type of resource in the use cycle, but many collections of them
+  #resource collections that have no owner perse, used for transfer
+  lot :gown_dirty_lot, "Gown Lot (dirty)", :gown 
+  lot :gown_clean, "Gown Lot (clean)", :gown 
+  lot :gown_ready_for_use, "Gown Lot", :gown 
+
   # OLVG
+  # Either i can make more than one agent with one user account, or each agent is tied to a user account
+  # Should be able to generate (fake) user accounts through graphql
   agent :a_hospital, "OLVG" do
-    init do
-      resource :gown_in_use, "Gown Lot (in use)", Resource::Pool, "400-500" # gowns in use in the hospital 
-      resource :gown_dirty_pool, "Gown (dirty)", Resource::Pool, "0" # gowns in the hamper in the hospital 
-    end
+      pool :gown_in_use, "Gown Lot (in use)", :gown, rand(400..500) # gowns in use in the hospital 
+      pool :gown_dirty_pool, "Gown (dirty)", :gown # gowns in the hamper in the hospital, defaults to zero
   end
  
   # Clean Lease
   agent :a_tsc, "Clean Lease Service" do 
-    init do
-      resource :gown_stock, "Gown (in stock)", Resource::Inventory, "1000" # in stock in the tsc
-    end
+      inventory :gown_stock, "Gown (in stock)", :gown, rand(800..1000) # in stock in the tsc
   end
 
   #this agent is considered 'jit', and only deals with lots
   # Clean Lease
   agent :a_launderer, "Clean Lease Laundry Service"
 
-  #resources that have now owner, used for transfer
-  resource :gown_dirty_lot, "Gown Lot (dirty)", Resource::Lot  
-  resource :gown_clean, "Gown Lot (clean)", Resource::Lot  
-  resource :gown_ready_for_use, "Gown Lot", Resource::Lot 
-
   # on delivery add the lot to the in use pool 
   # every day between 10 and 40 in use gowns are put in the hamper
   event :e_out_use, "Use (Discard)" do 
-    schedule :cron => "*/1", :amount => "10-40" do
-      role :a_hospital, Role::Performer
+    schedule 1 # every day
+    process do 
+      with_amount rand(10..40)
+      with_performer :a_hospital
       consume :gown_in_use # lower / remove resources from the in use pool
       produce :gown_dirty_pool # raise / add resources to  the dirty pool
     end
   end 
 
-  # every seven days the dirty pool is picked up
-  event :e_transfer_pickup, "pickup" do 
-    schedule :cron => "*/7" do
-      role :a_hospital, Role::Provider 
-      role :a_launderer, Role::Receiver 
-      consume :gown_dirty_pool # lowers the pool to 0
-      produce :gown_dirty_lot # with the total amount / the resources in the pool
-      transfer :gown_dirty_lot 
-    end
-  end 
+  # # every seven days the dirty pool is picked up
+  # event :e_transfer_pickup, "pickup" do 
+  #   schedule :cron => 7 do
+  #     role :a_hospital, Role::Provider 
+  #     role :a_launderer, Role::Receiver 
+  #     consume :gown_dirty_pool # lowers the pool to 0
+  #     produce :gown_dirty_lot # with the total amount / the resources in the pool
+  #     transfer :gown_dirty_lot 
+  #   end
+  # end 
 
-  # performs laundry for entire lot 1 day abter receiving :gown_dirty_lot
-  event :e_laundry, "Work (do laundry)" do
-    schedule :on => :e_transfer_pickup, :with_delay => "1d" do
-      role :a_launderer, Role::Performer 
-      consume :gown_dirty_lot
-      produce :gown_clean
+  # # performs laundry for entire lot 1 day abter receiving :gown_dirty_lot
+  # event :e_laundry, "Work (do laundry)" do
+  #   schedule :on => :e_transfer_pickup, :with_delay => 1 do
+  #     role :a_launderer, Role::Performer 
+  #     consume :gown_dirty_lot
+  #     produce :gown_clean
 
-      # the nr of time that the gown is washed should be updated
-    end
-  end 
+  #     # the nr of time that the gown is washed should be updated
+  #   end
+  # end 
 
-  # transfers entire lot between 0-2 days after cleaning to tsc
-  event :e_transfer_clean, "Transfer" do 
-    schedule :on => :e_laundry, :with_delay => "0-2d"  do
-      role :a_launderer, Role::Provider 
-      role :a_tsc, Role::Receiver 
-      transfer :gown_clean
-    end
-  end 
+  # # transfers entire lot between 0-2 days after cleaning to tsc
+  # event :e_transfer_clean, "Transfer" do 
+  #   schedule :on => :e_laundry, :with_delay => rand(0..2)  do
+  #     role :a_launderer, Role::Provider 
+  #     role :a_tsc, Role::Receiver 
+  #     transfer :gown_clean
+  #   end
+  # end 
 
-  # tcs inspect lot between 0-3 days after receiving
-  event :e_inspect, "Modify (QI)" do 
-    schedule :on => :e_transfer_clean, :with_delay => "0-3d" do
-      role :a_tsc, Role::Performer 
-      consume :gown_clean, "1.0" 
-      produce :gown_stock, "0.8-0.95" # between 0.05 and 0.2 of the lot doesn't pass inspection (should raise inventory)
-    end
-  end  
+  # # tcs inspect lot between 0-3 days after receiving
+  # event :e_inspect, "Modify (QI)" do 
+  #   schedule :on => :e_transfer_clean, :with_delay => rand(0..2) do
+  #     role :a_tsc, Role::Performer 
+  #     consume :gown_clean, "1.0" 
+  #     produce :gown_stock, "0.8-0.95" # between 0.05 and 0.2 of the lot doesn't pass inspection (should raise inventory)
+  #   end
+  # end  
 
-  # every day, take between 1 and 10 gowns from 
-  # the TSC stock and transfer them to the hospital
-  event :e_transfer_delivery, "Transfer (Delivery)" do 
-    schedule :cron => "*/1", :amount => "1-10" do
-      role :a_tsc, Role::Provider 
-      role :a_hospital, Role::Receiver 
-      consume :gown_stock
-      produce :gown_ready_for_use
-      transfer :gown_ready_for_use
-     end
-  end 
+  # # every day, take between 1 and 10 gowns from 
+  # # the TSC stock and transfer them to the hospital
+  # event :e_transfer_delivery, "Transfer (Delivery)" do 
+  #   schedule :cron => "*/1", :with_amount => rand(1..10) do
+  #     role :a_tsc, Role::Provider 
+  #     role :a_hospital, Role::Receiver 
+  #     consume :gown_stock
+  #     produce :gown_ready_for_use
+  #     transfer :gown_ready_for_use
+  #    end
+  # end 
 
-  # on delivery add the lot to the in use pool 
-  event :e_in_use, "Use (Wear)" do 
-    schedule :on => :e_transfer_delivery do
-      role :a_hospital, Role::Performer
-      consume :gown_ready_for_use
-      produce :gown_in_use #raise the in use pool
-    end
-  end 
+  # # on delivery add the lot to the in use pool 
+  # event :e_in_use, "Use (Wear)" do 
+  #   schedule :on => :e_transfer_delivery do
+  #     role :a_hospital, Role::Performer
+  #     consume :gown_ready_for_use
+  #     produce :gown_in_use #raise the in use pool
+  #   end
+  # end 
 
 end
