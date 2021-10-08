4b09e067d0b6cc427374cbd2c1b7e11e67d61e0d
Taco van Dijk <taco@waag.org>
2021-10-08 11:03:53 +0200

    swap shop agents

--- sim/swapshop_simple.rb
+++ sim/swapshop_simple.rb
@@ -1,5 +1,17 @@
 require_relative 'dsl.rb'
 
+# Simple scenario
+# Day 1
+# We have one customer who has 5 garments in their closet
+# Takes them to the swapshop, who inspects the garments
+# 4 of them make it to the rack, 1 is transferred to wieland as waste (garment consumed, waste produced) 
+# 3 other pieces of garment that are already in the rack are taken from the rack by the consumer, puts them in his closet (transfer)
+# Day 2
+# the consumer wears one of the new garments in his closet (use)
+# Day 3
+# the consumer repairs one of the new garments in his closet (transfer-custody by consumer to rep+ modify by repairshop)
+# One day after repair, the garment comes back to the consumer (transfer-custody rep back to consumer)
+#
 simulation("Swapshop Use Cycle", Date.today, Date.today + 30) do
   $rid = 0
   resource :garment, "Garment" do
@@ -15,12 +27,28 @@ simulation("Swapshop Use Cycle", Date.today, Date.today + 30) do
   agent :swapshop, "Swapshop Amsterdam" do
     authenticate "SWAPSHOP_EMAIL", "SWAPSHOP_PASSWORD"
     location "SWAPSHOP_LOCATION"
-    pool :garments, "fresh supply", :garment, 2
+    inventory :rack, "rack", :garment, 10
   end
 
-  #todo agent consumer 1-30  (would be great to script somehow)
-  
+  agent :consumer, "Sir Swapsalot" do
+    authenticate "CONS1_EMAIL", "CONS1_PASSWORD"
+    location "CONS1_LOCATION"
+    pool :closet, "closet", :garment, 5
+  end
+
+  agent :repairshop, "Miss Fixit" do
+    authenticate "REPAIRSHOP_EMAIL", "REPAIRSHOP_PASSWORD"
+    location "REPAIRSHOP_LOCATION"
+  end
+
+  # TODO agent consumer 1-30  (would be great to script somehow)
+  # i will start with one consumer for simplicity
   # pseudo code for events
+ 
+  # event consumer_swap_in
+  # as consumer
+  # pool_take :closet 1-5 
+  # transfer_batch consumer swapshop
   
   # event swap_inspect
   # cron 1
@@ -55,14 +83,7 @@ simulation("Swapshop Use Cycle", Date.today, Date.today + 30) do
   # on_event repair with_delay 1-5
   # pool_take
   # batch_transfer consumer
-  # etc.
-
-  # event consumer_swap
-  # as consumer
-  # pool_take :closet 1-5 
-  # transfer_batch consumer swapshop
-  
-  #etc
+ 
 end
 
 
