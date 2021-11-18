0fc3cca382f39379e487c378b6dffebd47a13f00
Taco van Dijk <taco@waag.org>
2021-11-18 16:12:23 +0100

    Resource seed

--- swapshop/pilot/swapstories/db/seeds.rb
+++ swapshop/pilot/swapstories/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Resource.create(title: 'Groen roze bloemenprint maxi rok',
+                description: 'Groen roze bloemenprint maxi rok, gemaakt van 100% polyester, maat onbekend (waarschijnlijk S), geplooid aan de voor- en achterkant en ritssluiting aan de linker zijkant',
+                image_url: 'https://cdn.webshopapp.com/shops/314295/files/384536825/750x1000x1/groen-roze-bloemenprint-maxi-rok.jpg',
+                tracking_id: 'RP00017',
+                shop_id: '998',
+                ros_id: nil)
+
+Resource.create(title: 'Blauw rode geruitte rok',
+                description: 'Blauw rode klokkende rok, maat onbekend (M), materiaal onbekend, klokkend model, ritssluiting met knoop',
+                image_url: 'https://cdn.webshopapp.com/shops/314295/files/384542229/image.jpg',
+                tracking_id: 'RP00018',
+                shop_id: '999',
+                ros_id: nil)
+
+Resource.create(title: 'Wit spetter print trainingsjack',
+                description: ' Witte spetter print trainingsjack, maat S (valt ruim), materiaal onbekend, gevoerd en heeft 2 zakken met ritsen',
+                image_url: 'https://cdn.webshopapp.com/shops/314295/files/384536080/image.jpg',
+                tracking_id: 'RP00019',
+                shop_id: '1000',
+                ros_id: nil)
+
