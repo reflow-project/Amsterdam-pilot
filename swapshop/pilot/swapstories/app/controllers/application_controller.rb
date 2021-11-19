ccb6aa2b6b22d2f26338b433c6f285251369800b
Taco van Dijk <taco@waag.org>
2021-11-19 13:45:20 +0100

    find resource by tracking id

--- swapshop/pilot/swapstories/app/controllers/application_controller.rb
+++ swapshop/pilot/swapstories/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def render_not_found
+    render :file => "#{Rails.root}/public/404.html",  :status => 404
+  end
+
+  def not_found
+      raise ActionController::RoutingError.new('Not Found') rescue render_not_found
+  end
+
 end
--- swapshop/pilot/swapstories/app/controllers/narratives_controller.rb
+++ swapshop/pilot/swapstories/app/controllers/narratives_controller.rb
@@ -1,4 +1,5 @@
 class NarrativesController < ApplicationController
   def show
+    @resource= Resource.where(["tracking_id = ?", params[:id]]).first or not_found
   end
 end
