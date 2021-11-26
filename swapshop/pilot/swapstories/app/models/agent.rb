94b5f16ca8bf26729d778a60fa2239975747dd22
Taco van Dijk <taco@waag.org>
2021-11-26 15:48:31 +0100

    dialog fsm

--- swapshop/pilot/swapstories/app/models/agent.rb
+++ swapshop/pilot/swapstories/app/models/agent.rb
@@ -4,6 +4,42 @@ module AgentType
 end
 
 class Agent < ApplicationRecord
+  validates :dialog_state, presence: true
+  before_validation :set_initial_state, on: :create
+
+  def set_initial_state
+    self.dialog_state = fsm.current
+  end
+
+  after_find :restore_state
+  after_initialize :restore_state
+
+  def restore_state
+    fsm.restore!(self.dialog_state.to_sym) if self.dialog_state.present?
+  end
+
+  def fsm
+    @fsm ||= FiniteMachine.new(self) do
+
+      initial :root
+
+      #swapshop register new item branch
+      event :register, :root => :r_title 
+      event :next, :r_title => :r_description 
+      event :next, :r_description => :r_photo
+      event :next, :r_photo => :root
+     
+      #participant swap new item branch
+      event :swap, :root => :s_q1
+      event :next, :s_q1 => :s_q2
+      event :next, :s_q2 => :s_q_photo
+      event :next, :s_q_photo => :root
+    
+      on_enter do |event|
+        target.dialog_state = event.to
+      end
+    end
+  end
 
   #finds or creates a new agent for a given telegram id
   def self.find_or_create_by_telegram_id(telegram_id)
@@ -29,4 +65,5 @@ class Agent < ApplicationRecord
     end
     self.save!
   end
+
 end
