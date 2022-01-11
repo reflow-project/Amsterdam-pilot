c81fe7d52cfcbf8b6d8a6d661b858fdcf50d52b8
Taco van Dijk <taco@waag.org>
2022-01-11 12:31:45 +0100

    skip forgery protection

--- swapshop/pilot/swapstories/app/controllers/application_controller.rb
+++ swapshop/pilot/swapstories/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+  skip_forgery_protection
   def render_not_found
     render :file => "#{Rails.root}/public/404.html",  :status => 404
   end
